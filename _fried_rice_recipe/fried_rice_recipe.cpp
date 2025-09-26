9a9cfc59593cf1b9da40bacf23318223bf43cbd7
Thomas Potthast <potthast@irz.uni-hannover.de>
2025-09-26 22:38:26 +0200

    change structure and play around be print a fried rice recipe in command line.

---
+++ _fried_rice_recipe/fried_rice_recipe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "This are the ingredients of my Fried Rice recipe:" << endl; // end line
+    cout << "Rice"
+         << "\nEgg \t 3"
+         << "\n Salt"
+         << "\n Pepper"
+         << "\n Garlic"
+         << "\n Oyster sauve";
+    return 0;
+}
